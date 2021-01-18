34630e44abc6465b15f83f39c5a9d693b3bfd6b8
Osama-you <osama.w.younis@gmail.com>
2021-01-18 15:47:07 +0200

    Add the ExerciseTwo Route

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import {Switch, Route, Link } from "react-router-dom";
 
 import './App.css';
 import ExerciseOne from "./component/ExerciseOne";
-// import ExerciseTwo from "./component/ExerciseTwo";
+import ExerciseTwo from "./component/ExerciseTwo";
 // import ExerciseThree from "./component/ExerciseThree";
 // import ExerciseFour from "./component/ExerciseFour";
 
@@ -12,17 +12,17 @@ function App() {
       <Switch>
           <Route exact path="/">
             <Link to='/ExerciseOne'>ExerciseOne</Link><br/>
-            {/* <Link to='/ExerciseTwo'>ExerciseTwo</Link><br/>
-            <Link to='/ExerciseThree'>ExerciseThree</Link><br/>
+            <Link to='/ExerciseTwo'>ExerciseTwo</Link><br/>
+            {/* <Link to='/ExerciseThree'>ExerciseThree</Link><br/>
             <Link to='/ExerciseFour'>ExerciseFour</Link><br/> */}
           </Route>
           <Route exact path="/ExerciseOne">
             <ExerciseOne />
           </Route>
-         {/* <Route exact path="/ExerciseTwo">
+          <Route exact path="/ExerciseTwo">
             <ExerciseTwo />
           </Route>
-          <Route exact path="/ExerciseThree">
+          {/*<Route exact path="/ExerciseThree">
             <ExerciseThree />
           </Route>
           <Route exact path="/ExerciseFour">
